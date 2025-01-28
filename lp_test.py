84d1f08d7f87756e7eb1d7cef1baa20640d2cc61
dhillr <dhillonrouzeau@gmail.com>
2025-01-28 16:31:08 -0500

    .

--- lp_test.py
+++ lp_test.py
@@ -4,7 +4,7 @@ from math import sin
 
 level = GD.GeometryDashLevel("lptest", "Example Username", "???", None, revision=0)
 def w(x): return (sin(x*0.02)*30)+(sin(x*0.08)*15)+(sin(x*0.16)*7)+15
-for i in range(1000):
+for i in range(10000):
     level.add_object(GD.GeometryDashObject(1, i+165, w(i), 0))
 
 GD.preview_level(level, Path(__file__)/"lp_test.py")
