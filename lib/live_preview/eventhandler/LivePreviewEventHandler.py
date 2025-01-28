a7f648ad8ca31b6067baa5aae2e5269c51048fee
dhillr <dhillonrouzeau@gmail.com>
2025-01-28 16:29:00 -0500

    .

--- lib/live_preview/eventhandler/LivePreviewEventHandler.py
+++ lib/live_preview/eventhandler/LivePreviewEventHandler.py
@@ -4,16 +4,19 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from pathlib import Path
 
+p = open(Path.cwd()/"lib"/"live_preview"/"eventhandler"/"path.txt", "r").read()
+open(Path.cwd()/"lib"/"live_preview"/"eventhandler"/"path.txt", "w")
+
 class LivePreviewEventHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        print(f"[LOG] Server restarting! ({event.src_path} was modified)")
+    def on_any_event(self, event):
+        print(event.event_type, event.src_path)
+        # print(f"[LOG] Server restarting! ({event.src_path} was modified)")
         subprocess.run(["python", "-m", "http.server", "8000"])
 
 if __name__ == "__main__":
-    event_handler = LivePreviewEventHandler()
+    eventHandler = LivePreviewEventHandler()
     observer = Observer()
-    p = open(Path.cwd()/"lib"/"live_preview"/"eventhandler"/"path.txt", "r").read()
-    observer.schedule(event_handler, path=p, recursive=True)
+    observer.schedule(eventHandler, path=p, recursive=True)
     observer.start()
 
     print("[LOG] Server started! (http://localhost:8000/lib/live_preview/index.html)")
